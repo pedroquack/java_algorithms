d7342a882750ad3a664402fc96539a1be90c133d
Pedro Volpatto <pedrovolpattocosta@gmail.com>
2024-08-04 19:49:25 -0300

    test

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,31 +1,36 @@
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] unorded_list = { 6, 3, 1, 19, 26, 99, 43, 132, 8, 35, 54, 69 };
-        System.out.println(Arrays.toString(unorded_list));
-        System.out.println(Arrays.toString(selection_sort(unorded_list)));
+        Integer[] array = new Integer[] {3,6,4,11,1,65,89,32,76,43,32,96,112};
+        List<Integer> list = new ArrayList<Integer>();
+        for (Integer i : array) {
+            list.add(i);
+        }
+        System.out.println(selection_sort(list));
     }
 
-
-    public static int lower_search(int[] list){
-        Integer lower = list[0];
+    public static Integer get_lowest(List<Integer> list){
+        Integer lower = list.get(0);
         Integer lower_index = 0;
-        for(int i = 0; i < list.length; i++){
-            if (list[i] < lower) {
-                lower = list[i];
+        for (int i = 0; i < list.size() -1; i++) {
+            if(list.get(i) > lower){
+                lower = list.get(i);
                 lower_index = i;
             }
         }
         return lower_index;
     }
 
-    public static int[] selection_sort(int[] list){
-        int[] ordered_list = new int[list.length];
-        for(int i = 0; i < list.length; i++){
-            int lower = lower_search(list);
-            ordered_list[i] = list[lower];
-            System.out.println(list[i]);
+    public static List<Integer> selection_sort(List<Integer> list){
+        List<Integer> ordered_list = new ArrayList<Integer>();
+
+        for (Integer i : list) {
+            Integer lower_index = get_lowest(list);
+            ordered_list.add(lower_index, i);
         }
         return ordered_list;
     }
